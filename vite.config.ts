7fc01942de4397762e8cdeff786aa1f0d2b9e4c8
Peter Nicholls <peterwnicholls@gmail.com>
2026-03-13 03:55:06 +0000

    Allow all hosts in Vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
+    allowedHosts: true,
     watch: {
       ignored: ["**/repos/**", "**/node_modules/**"],
     },
